9d70dddaab6c46bb665a885477c09b98194019e5
Gfelipe07 <141834936+Gfelipe07@users.noreply.github.com>
2024-05-10 11:04:55 -0300

    second commit

--- src/Estagiario.java
+++ src/Estagiario.java
@@ -1,27 +1,11 @@
-public class Estagiario extends Funcionario implements Trabalhavel {
-    private int horasTrabalho;
-    private String supervisor;
+public class Estagiario extends Funcionario {
 
-    public Estagiario(String nome, String matricula, int horasTrabalho, String supervisor) {
-        super(nome, matricula);
-        this.horasTrabalho = horasTrabalho;
-        this.supervisor = supervisor;
+    public Estagiario(String nome, int matricula, double salario) {
+        super(nome, matricula, salario);
     }
 
     @Override
-    public double calcularSalario() {
-        // Implemente o cálculo do salário para o estagiário
-        return 0.0; // implemente conforme a lógica necessária
-    }
-
-    // Implementação dos métodos da interface Trabalhavel
-    @Override
-    public void trabalhar() {
-        // Implemente a lógica para o trabalho de um estagiário
-    }
-
-    @Override
-    public void relatarProgresso() {
-        // Implemente a lógica para relatar o progresso de um estagiário
+    public String toString() {
+        return "Estagiário - " + super.toString();
     }
 }
