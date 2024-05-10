b6272a38bdaa3b71bdc095762e237877cedfc59c
Gfelipe07 <141834936+Gfelipe07@users.noreply.github.com>
2024-05-10 11:06:52 -0300

    second commits

--- src/Gerente.java
+++ src/Gerente.java
@@ -1,27 +1,13 @@
-public class Gerente extends Funcionario implements Trabalhavel {
-    private double bonusAnual;
-    private String equipe;
+public class Gerente extends Funcionario {
+    private String projeto;
 
-    public Gerente(String nome, String matricula, double bonusAnual, String equipe) {
-        super(nome, matricula);
-        this.bonusAnual = bonusAnual;
-        this.equipe = equipe;
+    public Gerente(String nome, int matricula, double salario, String projeto) {
+        super(nome, matricula, salario);
+        this.projeto = projeto;
     }
 
     @Override
-    public double calcularSalario() {
-        // Implemente o cálculo do salário para o gerente, considerando o bônus anual
-        return 0.0; // implemente conforme a lógica necessária
-    }
-
-    // Implementação dos métodos da interface Trabalhavel
-    @Override
-    public void trabalhar() {
-        // Implemente a lógica para o trabalho de um gerente
-    }
-
-    @Override
-    public void relatarProgresso() {
-        // Implemente a lógica para relatar o progresso de um gerente
+    public String toString() {
+        return "Gerente - " + super.toString() + ", Projeto: " + projeto;
     }
 }
